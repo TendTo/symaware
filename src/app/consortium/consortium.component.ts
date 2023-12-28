5da951bf69bdfa5c81d3583b443b78dbc2dde0b9
dovydas <dovydas.vilkevicius@gmail.com>
2023-12-28 19:32:54 +0000

    Update for big screens + added news.

--- src/app/consortium/consortium.component.ts
+++ src/app/consortium/consortium.component.ts
@@ -1,10 +1,26 @@
-import { Component } from '@angular/core';
+import { Component } from '@angular/core'
+import { Meta } from '@angular/platform-browser'
 
 @Component({
-  selector: 'app-consortium',
-  templateUrl: './consortium.component.html',
-  styleUrls: ['./consortium.component.scss']
+    selector: 'app-consortium',
+    templateUrl: './consortium.component.html',
+    styleUrls: ['./consortium.component.scss'],
 })
 export class ConsortiumComponent {
+    constructor(private meta: Meta) {}
 
+    ngOnInit(): void {
+        document.title = 'Consortium | SymAware'
+        this.meta.addTag({ name: 'title', content: 'Consortium | SymAware' })
+        this.meta.addTag({
+            name: 'description',
+            content:
+                'Page presents companies & institutions that contribute to the project.',
+        })
+    }
+
+    ngOnDestroy(): void {
+        this.meta.removeTag('name="title"')
+        this.meta.removeTag('name="description"')
+    }
 }
